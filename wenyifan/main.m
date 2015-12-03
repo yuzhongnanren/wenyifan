688e194c21511a4791095b7c442cbff79302a600
周勇 <zhouyong@haodai.com>
2015-12-03 09:54:52 +0800

    Initial Commit

---
+++ wenyifan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wenyifan
+//
+//  Created by haodai on 15/12/3.
+//  Copyright (c) 2015年 zhouyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
